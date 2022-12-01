c4bea5a6ec337777269c204907224273c439e9da
Ben Hayden <hayden767@gmail.com>
2022-12-01 09:44:40 -0600

    Add Visual Studio Community & C#

--- bendows-install.ps1
+++ bendows-install.ps1
@@ -23,6 +23,7 @@ try {
         winget install beekeeper-studio.beekeeper-studio -s winget
         winget install Putty.Putty -s winget
         winget install Microsoft.DotNet.SDK.6 -s winget
+        winget install "Visual Studio Community 2022"  --override "--add Microsoft.VisualStudio.Workload.ManagedDesktop Microsoft.VisualStudio.ComponentGroup.WindowsAppSDK.Cs" -s msstore
 
         Write-Output "Removing McAfee"
         winget uninstall MSC
