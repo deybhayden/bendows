e6167256075339e39aaf7f383dc5e0fe36ee7721
Ben Hayden <hayden767@gmail.com>
2023-02-22 14:26:51 -0600

    Add NodeJS

--- bendows-install.ps1
+++ bendows-install.ps1
@@ -24,6 +24,7 @@ try {
         winget install Microsoft.DotNet.SDK.6 -s winget
         winget install "Visual Studio Community 2022"  --override "--add Microsoft.VisualStudio.Workload.ManagedDesktop Microsoft.VisualStudio.ComponentGroup.WindowsAppSDK.Cs" -s msstore
         winget install GIMP.GIMP -s winget
+        winget install OpenJS.NodeJS.LTS -s winget
 
         Write-Output "Removing McAfee"
         winget uninstall MSC
