594d48f7c66df8bdd460e413434017f0798a5882
Ben Hayden <hayden767@gmail.com>
2025-02-17 11:45:01 -0600

    add mongosh

--- bendows-install.ps1
+++ bendows-install.ps1
@@ -27,6 +27,7 @@ try {
         winget install Armin2208.WindowsAutoNightMode -s winget
         winget install beekeeper-studio.beekeeper-studio -s winget
         winget install MongoDB.Compass.Full -s winget
+        winget install MongoDB.Shell -s winget
         winget install Putty.Putty -s winget
         winget install Microsoft.DotNet.SDK.6 -s winget
         winget install "Visual Studio Community 2022"  --override "--add Microsoft.VisualStudio.Workload.ManagedDesktop Microsoft.VisualStudio.ComponentGroup.WindowsAppSDK.Cs" -s msstore
